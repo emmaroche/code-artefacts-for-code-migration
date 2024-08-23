1571ec5a5ea311ccac2cc9a0abdeca798d61b516
Emma R <20088680@mail.wit.ie>
2024-08-23 15:19:35 +0100

    Add code artefacts

---
+++ Chosen Classes - Java to Kotlin/test/CheckVowelsTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckVowelsTest {
+
+    @Test
+    public void isVowel() {
+        assertTrue(CheckVowels.hasVowels("foo"));
+        assertTrue(CheckVowels.hasVowels("bar"));
+        assertFalse(CheckVowels.hasVowels("why"));
+        assertFalse(CheckVowels.hasVowels("myths"));
+    }
+}
